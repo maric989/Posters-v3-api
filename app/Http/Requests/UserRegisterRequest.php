cd3d32286d9c67f1f9e87658f1b4e7466d84c1ea
Djordje Maric <djordje.maric@quantox.com>
2019-11-29 15:39:21 +0100

    Migration added, user auth added

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'A title is required',
+            'email.required'  => 'A message is required',
+        ];
+    }
+
+}
